2361de898e1c28e2d94f27c22a6c31d306319769
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-04-04 14:58:17 +0200

    Created bin/swimmingpool.cpp

---
+++ bin/swimmingpool.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file swimmingpool.cpp
+ *
+ * @version 01.01 202144
+ *
+ * @brief https://training.olinfo.it/#/task/ois_swimmingpool/statement
+ *
+ * @ingroup swimmingpool
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
